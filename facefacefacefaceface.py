1592f5d7d46a5d4184909d6f8e68b847d67ffc6b
fumifumi55 <m1m23r24@oit.ac.jp>
2023-07-27 20:28:56 +0900

    動画終了時にプログラムを終了させる変更を追加

--- facefacefacefaceface.py
+++ facefacefacefaceface.py
@@ -1,20 +1,27 @@
-# ①モジュールをインポートする
+# 必要なモジュールをインポートする
 import requests
 import base64
 import json
 import pprint
 import cv2
-import time
 
-# ②動画ファイルの読み込み
-video_path = "/Users/fumi/PycharmProjects/pythonProject/multimodal/a2g1n-20lik.mp4"  # 読み込ませたい動画ファイルを入力する
+# 動画ファイルのパスを指定する
+video_path = "/Users/fumi/PycharmProjects/pythonProject/multimodal/a2g1n-20lik.mp4"  # 自分の動画ファイルのパスに変更してください
 cap = cv2.VideoCapture(video_path)
 
-# ③フレームごとに動画を読み込み、画像として表情分析を行う
+# 表情認識用のAPI情報を設定する
 endpoint = 'https://api-us.faceplusplus.com'
 api_key = "SK7WRvwNYjP5cVHQqzKNcUEU1J7PzxX3"  # ご自身の「API Key」を入力する
 api_secret = "9ZLk4Teaxa0l1USu-EuCfJ_Sgv6YbdyN"  # ご自身の「API Secret」を入力する
 
+# 動画のフレーム数を取得する
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+# フレームレートを取得する
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+# 1秒ごとに表情認識を行い、動画を全て認識するループ
+seconds_passed = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -39,9 +46,16 @@ while cap.isOpened():
         if len(json_dict['faces']) > 0:
             pprint.pprint(json_dict['faces'][0]['attributes']['emotion'])
 
-    # 0.5秒ごとに停止する（必要に応じて調整）
-    time.sleep(1)
+    # 1秒待機する
+    cv2.waitKey(1000)
 
-# ④後処理としてキャプチャをリリースする
+    # 経過時間を更新
+    seconds_passed += 1
+
+    # 動画を全て認識したらループを抜ける
+    if seconds_passed >= frame_count / fps:
+        break
+
+# 後処理としてキャプチャをリリースする
 cap.release()
 cv2.destroyAllWindows()
